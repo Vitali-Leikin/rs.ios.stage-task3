c426e08f0e01404e0edf11df65173aad3dc2bacf
Vitali-Leikin <leikin1.19@gmail.com>
2021-05-23 16:36:18 +0300

    init:  release

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,47 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    
+    
+    NSInteger counter = 0;
+    NSInteger n = ratingArray.count;
+    
+    if (ratingArray.count <= 2 || ratingArray == nil) {
+        return  0;
+    }
+    
+    for (NSInteger i = 0; i < ratingArray.count - 2 ; i++) {
+        for (NSInteger j = 1; j < ratingArray.count - 1 ; j++) {
+            for (NSInteger k = 2; k < ratingArray.count ; k++) {
+                
+                
+                if (([ratingArray[i] intValue] < [ratingArray[j] intValue ]) && ([ratingArray[j] intValue ] < [ratingArray[k] intValue ]))   {
+                    NSLog(@"%d", [ratingArray[i] intValue]);
+                    NSLog(@"%d", [ratingArray[j] intValue]);
+                    NSLog(@"%d", [ratingArray[k] intValue]);
+                    
+                    if ((0 <= i) && (i < j) && (j < k) && (k < n)) {
+                        counter += 1;
+                    }
+                    
+                } else if (([ratingArray[i] intValue] > [ratingArray[j] intValue ]) && ([ratingArray[j] intValue ] > [ratingArray[k] intValue ])) {
+                    
+                    
+                    if ((0 <= i) && (i < j) && (j < k) && (k < n)){
+                        counter += 1;
+                    }
+                    
+                }
+                                
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    return counter;
 }
 
 @end
--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,23 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    NSMutableString *resultStr = [[NSMutableString alloc] init];
+
+    while ([string1 length] != 0 && [string2 length] != 0) {
+        
+        if ([string1 UTF8String][0] < [string2 UTF8String][0] || [string1 UTF8String][0] == [string2 UTF8String][0]) {
+            [resultStr appendString: [string1 substringToIndex: 1]];
+            string1 = [string1 substringFromIndex: 1];
+        } else {
+            [resultStr appendString: [string2 substringToIndex: 1]];
+            string2 = [string2 substringFromIndex: 1];
+        }    }
+
+    [string1 length] == 0 ? [resultStr appendString: string2] : [resultStr appendString: string1] ;
+    
+
+    
+    return resultStr;
 }
 
 @end
