ce05cd9a2cc7591195dce58d62c5361a08a19d6f
Vitali-Leikin <74469045+Vitali-Leikin@users.noreply.github.com>
2021-05-24 21:05:49 +0300

    Update LevelOrderTraversal.m

--- Objc_Task3/ios.stage-task/Exercises/Exercise-4/LevelOrderTraversal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-4/LevelOrderTraversal.m
@@ -1,5 +1,45 @@
 #import "LevelOrderTraversal.h"
 
+typedef enum {
+    sonLeft, sonRight
+} Node;
+
+NSUInteger levelTreeResult(NSArray *tree, NSMutableArray *levelOrderedData, NSUInteger counter, NSUInteger item);
+
+
 NSArray *LevelOrderTraversalForTree(NSArray *tree) {
-    return nil;
+    
+    NSMutableArray *arrayLevelTree = [NSMutableArray new];
+    
+    levelTreeResult(tree, arrayLevelTree, 0, 0);
+    
+    return arrayLevelTree;
 }
+
+
+
+
+NSUInteger levelTreeResult(NSArray *tree, NSMutableArray *levelOrderedData, NSUInteger level, NSUInteger item) {
+    
+    NSUInteger result = item;
+    NSMutableArray *mutArray = [NSMutableArray new];
+    
+    for (Node nodeType = 0; nodeType <= sonRight && result < tree.count; ++nodeType) {
+        
+        NSObject *node = tree[result++];
+        
+        if ([node isKindOfClass:NSNumber.class]) {
+            
+            if (levelOrderedData.count == level) {
+                
+                [levelOrderedData addObject:mutArray];
+            }
+            
+            [levelOrderedData[level] addObject:node];
+            
+            result = levelTreeResult(tree, levelOrderedData, level+1, result);
+        }
+    }
+    return result;
+}
+
